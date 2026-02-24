1fc0514ada89d5fa893d6778086a1c047b5a00a6
Gustavo-HDM <gustavo.h.d.marques@gmail.com>
2026-02-24 10:46:34 -0300

    Banco de dados Mongo funcionando

--- library/src/main/java/com/activity/library/api/entity/Book.java
+++ library/src/main/java/com/activity/library/api/entity/Book.java
@@ -16,27 +16,4 @@ public class Book {
     private String author;
     private String ISBN;
     private BigDecimal price;
-
- /*   // ===// CONSTRUTORES //===
-    public Book() {}
-
-    public Book(String id, String title, String author, String ISBN, BigDecimal price) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.ISBN = ISBN;
-        this.price = price;
-    }
-
-    // ===// GETTERS & SETTERS //===
-    /*public String getId() {return id;}
-    public void setId(String id) {this.id = id;}
-    public String getTitle() {return title;}
-    public void setTitle(String title) {this.title = title;}
-    public String getAuthor() {return author;}
-    public void setAuthor(String author) {this.author = author;}
-    public String getISBN() {return ISBN;}
-    public void setISBN(String ISBN) {this.ISBN = ISBN;}
-    public BigDecimal getPrice() {return price;}
-    public void setPrice(BigDecimal price) {this.price = price;}*/
 }
